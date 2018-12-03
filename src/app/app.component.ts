3aa8aaf58cf6f718dd38b502e65b54322487af0a
Rodrigo Beltran <rodrich.bb@gmail.com>
2018-12-03 08:21:39 -0400

    productos commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'productos-angular-lab';
+  title = "productos-angular-lab";
+
+  datos = [
+    { codigo: 1, descripcion: "papas", precio: 10.55 },
+    { codigo: 1, descripcion: "manzanas", precio: 12.1 },
+    { codigo: 1, descripcion: "melon", precio: 52.3 },
+    { codigo: 1, descripcion: "cebollas", precio: 17 },
+    { codigo: 1, descripcion: "calabaza", precio: 20 }
+  ];
 }
